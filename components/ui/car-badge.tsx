568a7154a53dd073ae37b5893be5520edc033a9c
yousri <129407665+yousriB@users.noreply.github.com>
2025-10-29 15:55:09 +0100

    change prix and badge style

--- components/ui/car-badge.tsx
+++ components/ui/car-badge.tsx
@@ -15,16 +15,21 @@ export default function CarBadge({
 
   return (
     <div
-      className={`absolute top-0  flex flex-row px-1 z-10  w-full justify-between  ${className}`}
+      className={`absolute top-0 flex flex-row px-1 z-10 w-full justify-between ${className}`}
     >
       {isNew && (
-        <span className="bg-red-600 text-white text-xs font-semibold px-3 py-1 rounded-full shadow-md uppercase tracking-wider">
+        <span className="relative bg-green-800 text-white text-xs font-semibold px-3 py-1 rounded-xl shadow-md uppercase tracking-wider">
           Nouveau
+          {/* Triangle bottom-left */}
+          <div className="absolute -bottom-1 left-2 w-0 h-0 border-l-8 border-l-transparent border-t-8 border-t-green-800"></div>
         </span>
       )}
+
       {isPromotion && (
-        <span className="bg-black text-white text-xs font-semibold px-3 py-1 rounded-full shadow-md uppercase tracking-wider">
-          Promotion
+        <span className="relative bg-red-600 text-white text-xs font-semibold px-3 py-1 rounded-xl shadow-md uppercase tracking-wider">
+          Promo
+          {/* Triangle bottom-right */}
+          <div className="absolute -bottom-1 right-2 w-0 h-0 border-r-8 border-r-transparent border-t-8 border-t-red-600"></div>
         </span>
       )}
     </div>
